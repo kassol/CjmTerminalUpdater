e1bb861f0a3b11f036b62ae6a8a10b021a7b8da8
kassol <kassol.zx@gmail.com>
2015-08-31 16:11:21 +0800

    Add download part.

---
+++ CjmTerminalUpdater.pro
@@ -0,0 +1,24 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-08-31T14:37:28
+#
+#-------------------------------------------------
+
+QT       += core gui network
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = CjmTerminalUpdater
+TEMPLATE = app
+
+
+SOURCES += main.cpp \
+    downloadmanagerHTTP.cpp
+
+HEADERS  += \
+    downloadmanagerHTTP.h
+
+FORMS    +=
+
+RESOURCES += \
+    resource.qrc
---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <QApplication>
+#include <QSystemTrayIcon>
+#include <QMenu>
+#include <QIcon>
+#include <QAction>
+#include <QTextCodec>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QTextCodec::setCodecForTr(QTextCodec::codecForName("GBK"));
+    QTextCodec::setCodecForCStrings(QTextCodec::codecForName("GBK"));
+
+    QSystemTrayIcon *trayicon = new QSystemTrayIcon();
+
+    QIcon icon(":/update.ico");
+    trayicon->setIcon(icon);
+
+    QMenu *trayiconMenu = new QMenu();
+
+    QAction *updateAction = new QAction(QObject::tr("显示主窗口"), NULL);
+    QAction *quitAction = new QAction(QObject::tr("退出"), NULL);
+    QObject::connect(quitAction, SIGNAL(triggered()), qApp, SLOT(quit()));
+
+    trayiconMenu->addAction(updateAction);
+    trayiconMenu->addAction(quitAction);
+    trayicon->setContextMenu(trayiconMenu);
+
+    trayicon->show();
+
+    return a.exec();
+}
